e176d2fdee25bb14a9be995c0c9269ed3f5a288d
rajdip <rajdips@gmail.com>
2013-05-27 22:58:12 +0530

    Initiating maven project

---
+++ src/main/java/org/in/hadoop/Test.java
@@ -0,0 +1,11 @@
+package org.in.hadoop;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: traw
+ * Date: 5/27/13
+ * Time: 10:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
